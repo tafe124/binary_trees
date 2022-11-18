af648e5502992c686ded6324413304671ba0daef
tafe124 <tafesegechera22@gmail.com>
2022-11-18 00:32:23 -0800

    redone

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -29,4 +29,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
